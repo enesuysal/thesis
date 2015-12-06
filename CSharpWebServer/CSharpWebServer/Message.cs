5033ff6030faeb59d83514265bb93ed056aba0e5
aa <you@example.com>
2015-12-06 21:33:01 +0000

    Add C# Server

---
+++ CSharpWebServer/CSharpWebServer/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpWebServer
+{
+    public class Message {
+    int messageInt;
+    String messageString;
+    Boolean messageBool;
+    Object messageObject;
+    public byte[] messageSerilized;
+    public Message(int message){
+        this.messageInt = message;
+        messageSerilized = CentralSerializer.intToByteArray(messageInt, messageSerilized);
+    }
+    public Message (String message){
+        this.messageString = message;
+        messageSerilized = CentralSerializer.stringToByteArray(message, messageSerilized);
+    }
+     public Message (Boolean message){
+        this.messageBool = message;
+        messageSerilized =CentralSerializer.boolToByteArray(message, messageSerilized);
+    }
+      public Message (Object message){
+        this.messageObject = message;
+        messageSerilized = CentralSerializer.objectToByteArray(message, messageSerilized);
+       
+    }
+   public string Seriliaze(){
+      string temp_inBase64 = Convert.ToBase64String(messageSerilized);
+       return temp_inBase64;
+   } 
+}
+}
---
+++ CSharpWebServer/ConsoleTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using CSharpWebServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object Class TEst
+        Test testObject = new Test(1, "Test");
+        Message msg1 = new Message(testObject);
+       
+        //Deserialize
+        Console.WriteLine( Receiver.GetMessage(msg1.Seriliaze()).GetType());
+        CallWebServerClient.CallWebServerClient service = new CallWebServerClient.CallWebServerClient();
+        Test newTestObject = (Test)service.GetResult(msg1.Seriliaze()); 
+        // Console.WriteLine(newTestObject.getTestName());
+       
+         
+// Test for String
+        String test = "fdfdfd"; 
+        Message msg2 = new Message(test);
+        //Deserialize
+        Console.WriteLine( Receiver.GetMessage(msg2.Seriliaze()).GetType());
+        String newTestString = (String)service.GetResult(msg2.Seriliaze());
+         Console.WriteLine(newTestString);
+         
+         
+          
+// Test for Int
+        int testInt = 55; 
+        Message msg3 = new Message(testInt);
+        //Deserialize
+        Console.WriteLine( Receiver.GetMessage(msg3.Seriliaze()).GetType());
+        int newTestInt = (int)service.GetResult(msg3.Seriliaze());
+         Console.WriteLine(newTestInt);
+// Test for Bool
+        Boolean testBool = true; 
+        Message msg4 = new Message(testBool);
+        //Deserialize
+        Console.WriteLine( Receiver.GetMessage(msg4.Seriliaze()).GetType());
+        Boolean newTestBool = (Boolean)service.GetResult(msg4.Seriliaze());
+         Console.WriteLine(newTestBool);
+         Console.ReadLine();
+        }
+    }
+}
