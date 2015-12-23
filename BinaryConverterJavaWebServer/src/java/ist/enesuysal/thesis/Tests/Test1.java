2fe77744a4d759dbd2839d56865eb6b8f8602024
Enes UYSAL <enesuysal@gmail.com>
2015-12-23 21:16:44 +0000

    First Step

---
+++ BinaryConverterJavaWebServer/src/java/ist/enesuysal/thesis/Tests/Test1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ist.enesuysal.thesis.Tests;
+
+import ist.enesuysal.thesis.Annotation.Mandatory;
+
+/**
+ *
+ * @author enes
+ */
+public class Test1 {
+
+    @Mandatory
+    public String count = "";
+    @Mandatory
+    public int version;
+    @Mandatory
+    public boolean deneme;
+    public int test = 66;
+}
