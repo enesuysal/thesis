12a3788019ce9c71c1ea44d1fe2f070a2dbc76ea
aa <you@example.com>
2016-02-20 21:42:59 +0000

    New Commit

---
+++ CSharpWebServer/RESTfulWebService/IRESTService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTfulWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRESTService" in both code and config file together.
+    [ServiceContract]
+    public interface IRESTService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml,
+                                   BodyStyle = WebMessageBodyStyle.Bare,
+                                   UriTemplate = "GetResult/{value}")]
+        string GetResult(string value);
+    }
+}
