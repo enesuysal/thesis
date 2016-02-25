a1cfbfae36197f2bebe625691ca2dcc3ce631ee3
aa <you@example.com>
2016-02-25 19:26:49 +0000

    New Commit

---
+++ CSharpWebServer/CSharpClientTestApp/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CSharpClientTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Java SOAP WebServices",
+            "C# SOAP WebServices",
+            "Java RESTful WebServices",
+            "C# RESTful WebServices"});
+            this.comboBox1.Location = new System.Drawing.Point(99, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Type:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "byte",
+            "short",
+            "int",
+            "long",
+            "float",
+            "double",
+            "char",
+            "string",
+            "boolean",
+            "ExampleClass1",
+            "ExampleClass2",
+            "ExampleClass3"});
+            this.comboBox2.Location = new System.Drawing.Point(99, 36);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(178, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Primitive Value:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 144);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(403, 157);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result returned from Server:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Send to Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 338);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Server TEST C# Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CSharpWebServer/CSharpClientTestApp/Form1.cs
@@ -0,0 +1,138 @@
+﻿using CSharpClientTestApp.CSharpWebServiceReference;
+using CSharpClientTestApp.JavaWebServiceReference;
+using CSharpWebServer.ist.enesuysal.thesis.Tests;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpClientTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      private void button1_Click(object sender, EventArgs e)
+        {
+            bool valid = false;
+            if (comboBox1.SelectedIndex == -1)
+                valid = false;
+            else if (comboBox2.SelectedIndex == -1)
+                valid = false;
+            else
+                valid = true;
+
+            if (!valid)
+            {
+                textBox2.Text = "Please Select Parameters";
+            }
+            else
+            {
+                TestSerial test = new TestSerial();
+                CSharpWebServer.ist.enesuysal.thesis.Message msg = new CSharpWebServer.ist.enesuysal.thesis.Message(test);
+                string msgToSend = msg.Seriliaze();
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        textBox2.Text = GetJavaWebService(msgToSend);
+                        
+                        break;
+                    case 1:
+                        textBox2.Text = GetCSharpWebService(msgToSend);
+                       
+                        break;
+                     case 2:
+                        textBox2.Text = "Java RESTful WebServices";
+                        GetRestMessage(msgToSend);
+                        break;
+                     case 3:
+                        textBox2.Text = "C# RESTful WebServices";
+                        GetRestMessage(msgToSend);
+                        break;
+                    default:
+                        break;
+                }
+    
+
+
+
+            }
+           
+
+        }
+        //http://www.codeproject.com/Articles/255684/Create-and-Consume-RESTFul-Service-in-NET-Framewor
+        public string GetRestMessage(string message)
+        {
+
+            try
+            {
+                string content;
+               // Console.WriteLine("Enter Method:");
+                string Method = "GET";//Console.ReadLine();
+
+                //Console.WriteLine("Enter URI:");
+                string uri = Console.ReadLine();
+
+                HttpWebRequest req = WebRequest.Create(uri) as HttpWebRequest;
+                req.KeepAlive = false;
+                req.Method = Method.ToUpper();
+
+                if (("POST,PUT").Split(',').Contains(Method.ToUpper()))
+                {
+                   
+                    content = message;
+
+                    byte[] buffer = Encoding.ASCII.GetBytes(content);
+                    req.ContentLength = buffer.Length;
+                    req.ContentType = "text/xml";
+                    Stream PostData = req.GetRequestStream();
+                    PostData.Write(buffer, 0, buffer.Length);
+                    PostData.Close();
+                }
+
+                HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+
+                Encoding enc = System.Text.Encoding.GetEncoding(1252);
+                StreamReader loResponseStream =
+                new StreamReader(resp.GetResponseStream(), enc);
+
+                string Response = loResponseStream.ReadToEnd();
+
+                loResponseStream.Close();
+                resp.Close();
+                return Response;
+            }
+            catch (Exception ex)
+            {
+              return (ex.Message.ToString());
+            }
+
+             
+        }
+        public string GetJavaWebService(string message)
+        {
+            JavaWebServiceClient service = new JavaWebServiceClient();
+            // Get result from server
+            return (service.GetResult(message));
+             
+        }
+
+        public string GetCSharpWebService(string message)
+        {
+            WebServiceSoapClient service = new WebServiceSoapClient();
+            // Get result from server
+            return ((string)service.GetResult(message));
+
+        }
+    }
+}
